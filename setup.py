fb02d28ffaf427dc297947769f828784c46df040
Your Name <shivamm389@gmail.com>
2024-01-21 23:17:51 +0000

    Fix github workflows

--- setup.py
+++ setup.py
@@ -17,5 +17,7 @@ setup(
     install_requires=[
         'thrift',
     ],
-    zip_safe=False
+    zip_safe=False,
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
 )
